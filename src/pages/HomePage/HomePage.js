c4154bac3b2ffb033b1ec7ea9cbc3489dc5d0a8d
Ahsan Shibghotullah <albarkahdev@gmail.com>
2021-11-14 20:09:35 +0700

    add button loadMorePokemons

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,4 +1,4 @@
-import { useEffect, useContext, Suspense } from "react";
+import { useEffect, useContext, Suspense, useState } from "react";
 
 import { GlobalContext } from '../../contexts/GlobalStateProvider';
 import { PokemonActions } from "../../actions";
@@ -18,7 +18,8 @@ const CardsPokemon = ({ pokemons }) => {
 }
 
 function HomePage() {
-  const [{ pokemons }, dispatch] = useContext(GlobalContext);
+  const [loadingMore, setLoadingMore] = useState(false);
+  const [{ pokemons, nextPokemonURL }, dispatch] = useContext(GlobalContext);
 
   useEffect(() => {
 
@@ -33,15 +34,56 @@ function HomePage() {
   fetchApiPokemons();
   }, []);
 
+  useEffect(() => {
+    setLoadingMore(true);
+    let idTimeOut = setTimeout(() => {
+      setLoadingMore(false);
+    }, 1000);
+
+    return () => idTimeOut && clearTimeout(idTimeOut);
+
+  }, [nextPokemonURL]);
+
+  const handleGetMorePokemons = async () => {
+    try {
+      setLoadingMore(true);
+      const result = await PokemonActions.getMorePokemons(
+        {
+          nextPokemonURL,
+          currentPokemons: pokemons,
+        },
+        dispatch
+      );
+    } catch (err) {
+      console.log({err});
+    } finally {
+      setLoadingMore(false);
+    }
+  }
+
   return (
-    <Suspense fallback={<p>Loading HomePage...</p>}>
-      <div className={styles.container}>
-        <h1>POKEDEX</h1>
-        <Suspense fallback={<p>Loading Card Image...</p>}>
-          <CardsPokemon pokemons={pokemons} />
-        </Suspense>
+    <>
+      <Suspense fallback={<p>Loading HomePage...</p>}>
+        <div className={styles.container}>
+          <h1>POKEDEX</h1>
+          <Suspense fallback={<p>Loading Card Image...</p>}>
+            <CardsPokemon pokemons={pokemons} />
+          </Suspense>
+        </div>
+      </Suspense>
+      <div className={styles.footer}>
+        {
+        nextPokemonURL && !loadingMore
+        ? (<div className={styles.button_load_more} onClick={handleGetMorePokemons}>
+            <p>Load more pokemon</p>
+          </div>)
+        : loadingMore && pokemons && (<div className={styles.button_load_more_loading}>
+          <p>Loading..</p>
+        </div>)
+        }
       </div>
-    </Suspense>
+      
+    </>
   )
 }
 
