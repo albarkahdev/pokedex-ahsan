bf451bc902c12a3a37244de15d0b0a0ba2925e5e
Ahsan Shibghotullah <albarkahdev@gmail.com>
2021-11-14 17:47:05 +0700

    apply lato font

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import DetailPage from "./pages/DetailPage/DetailPage";
 
-import './App.css';
+import styles from './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/:pokemonId" element={<DetailPage />} />
